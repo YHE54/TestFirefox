9d7df64491d66af4abbdc41ec925113357b2f0a3
YHE54 <heyuan0755@gmail.com>
2017-02-02 11:08:11 -0500

    Update Camic.js

--- caMicroscopeTestSuite/test/pageobjects/Camic.js
+++ caMicroscopeTestSuite/test/pageobjects/Camic.js
@@ -3,7 +3,7 @@ function Camic() {
 }
 
 Camic.prototype.open = function(path){
-    browser.url('http://snake.bmi.emory.edu:1337/camicroscope/osdCamicroscope.php?tissueId=CMUz');
+    browser.url('http://localhost:1337/camicroscope2/osdCamicroscope.php?tissueId=CMU-1-Small-Region');
 };
 
 module.exports = new Camic();
