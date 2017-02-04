2ec2acbb2cc3d394192a6c6caa3cce822133f146
YHE54 <heyuan0755@gmail.com>
2017-02-04 16:28:17 -0500

    Update wdio.conf.js

--- caMicroscopeTestSuite/wdio.conf.js
+++ caMicroscopeTestSuite/wdio.conf.js
@@ -42,9 +42,9 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instance available you can make sure that not more than
         // 5 instance gets started at a time.
-        //maxInstances: 5,
+        maxInstances: 5,
         //browserName=chrome,maxInstances=5,platform=LINUX
-        browserName: 'firefox' 
+        browserName: 'firefox',
         version: '46.0'
     }],
     //
