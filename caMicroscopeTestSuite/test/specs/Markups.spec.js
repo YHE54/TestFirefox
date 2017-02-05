6ca5c97b289ffe078cd17a4e9ec1ea0574eb71cd
YHE54 <heyuan0755@gmail.com>
2017-02-05 12:48:12 -0500

    Update Markups.spec.js

--- caMicroscopeTestSuite/test/specs/Markups.spec.js
+++ caMicroscopeTestSuite/test/specs/Markups.spec.js
@@ -3,8 +3,6 @@ var ToolBar = require("../pageobjects/ToolBar.js");
 var assert = require('chai').assert;
 var webdriverio = require('webdriverio');
 var options = { desiredCapabilities : { browserName: 'firefox' } };
-
-options.binary_location = '/usr/bin/chromium-browser';
 var client = webdriverio.remote(options);
 var Utils = require("../pageobjects/Utils.js"); //Pulls new Utils function set
 
