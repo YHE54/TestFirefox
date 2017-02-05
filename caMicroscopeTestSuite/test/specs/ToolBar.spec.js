b51f4aec8b35d62fbe9b3768a0afdd319fc9a9bc
YHE54 <heyuan0755@gmail.com>
2017-02-04 22:11:16 -0500

    Update ToolBar.spec.js

--- caMicroscopeTestSuite/test/specs/ToolBar.spec.js
+++ caMicroscopeTestSuite/test/specs/ToolBar.spec.js
@@ -2,7 +2,7 @@
 var ToolBar = require("../pageobjects/ToolBar.js");
 var assert = require('chai').assert;
 var webdriverio = require('webdriverio');
-var options = { desiredCapabilities : { browserName: 'firefox' };
+var options = { desiredCapabilities : { browserName: 'firefox' } };
 options.binary_location = '/usr/bin/chromium-browser';
 var client = webdriverio.remote(options);
 
